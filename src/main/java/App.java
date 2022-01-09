973927a6492b1c2f094ec8cda2c93d3188b5b3b2
Julian Bokelmann <J.B.Bokelmann@gmail.com>
2022-01-09 16:45:52 +0100

    :construction: test Apache Arrow JNI

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,20 @@
+import org.apache.arrow.plasma.PlasmaClient;
 import server.InfinimumDBServer;
 
 public class App {
-    public static void main(String[] args) throws InterruptedException {
-        InfinimumDBServer server = new InfinimumDBServer("/home/julian/Documents/Masterarbeit/Plasma-Examples/plasma", 4321);
+    public static void main(String[] args) {
+        //InfinimumDBServer server = new InfinimumDBServer("/home/julian/Documents/Masterarbeit/Plasma-Examples/plasma", 4321);
 
-        Object id = server.putString("Bla");
+        /*Object id = server.putString("Bla");
         System.out.println(server.getString(id));
 
         Thread.sleep(5000);
 
         Object id2 = server.putString("Bla2");
         System.out.println(server.getString(id2));
+        */
+        System.loadLibrary("plasma_java");
+        String plasmaFilePath = "/home/julian/Documents/Masterarbeit/Plasma-Examples/plasma";
+        PlasmaClient client = new PlasmaClient(plasmaFilePath, "", 0);
     }
 }
