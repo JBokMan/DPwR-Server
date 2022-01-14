9a3849821e1a8e462f5cbd2b0c933b889c6e2806
Julian Bokelmann <J.B.Bokelmann@gmail.com>
2022-01-14 22:14:35 +0100

    :sparkles: expand demo code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,8 @@ import java.util.UUID;
 public class App {
     public static void main(String[] args) {
         String plasmaFilePath = "/home/julian/Documents/Masterarbeit/Plasma-Examples/plasma";
-        InfinimumDBServer server = new InfinimumDBServer(plasmaFilePath, 4321);
+        InfinimumDBServer server = new InfinimumDBServer(plasmaFilePath, "127.0.0.1", 4321);
+        server.listen();
 
         String test = "test";
         System.out.println("Value to safe: " + test);
