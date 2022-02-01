6eb6d01d97a5ba01b151d01793d080bc1e39f6dd
Julian Bokelmann <J.B.Bokelmann@gmail.com>
2022-02-01 09:15:36 +0100

    :truck: move and rename App class

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,7 +1,9 @@
+package main;
+
 import server.InfinimumDBServer;
 
-public class App {
-    public static void main(String[] args) {
+public class Application {
+    public static void main(final String[] args) {
         final String plasmaFilePath = "/home/julian/Documents/Masterarbeit/InfinimumDB-Server/plasma";
         final InfinimumDBServer server = new InfinimumDBServer(plasmaFilePath, "127.0.0.1", 2998);
         server.listen();
