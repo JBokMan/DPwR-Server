9beaf4e3abb2aa18a7f1d9d78df627966a27f95f
Julian Bokelmann <J.B.Bokelmann@gmail.com>
2022-04-07 12:35:40 +0200

    :recycle: change demo code

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -2,12 +2,10 @@ package main;
 
 import server.DPwRServer;
 
-import java.net.UnknownHostException;
-
 public class Application {
     private final static String PLASMA_FILE_PATH = "/home/julian/Documents/Masterarbeit/InfinimumDB-Server/plasma";
 
-    public static void main(final String[] args) throws UnknownHostException {
+    public static void main(final String[] args) {
         final DPwRServer server = new DPwRServer(PLASMA_FILE_PATH, "127.0.0.1", 2998);
         server.listen();
     }
--- src/main/java/main/ApplicationSecondary.java
+++ src/main/java/main/ApplicationSecondary.java
@@ -4,14 +4,14 @@ import de.hhu.bsinfo.infinileap.binding.ControlException;
 import server.DPwRServer;
 
 import java.net.ConnectException;
-import java.net.UnknownHostException;
 import java.util.concurrent.TimeoutException;
 
 public class ApplicationSecondary {
     private final static String PLASMA_FILE_PATH = "/home/julian/Documents/Masterarbeit/InfinimumDB-Server/plasma";
 
-    public static void main(final String[] args) throws UnknownHostException, ControlException, TimeoutException, ConnectException {
+    public static void main(final String[] args) throws ControlException, TimeoutException, ConnectException {
         final DPwRServer server = new DPwRServer(PLASMA_FILE_PATH, "127.0.0.1", 2997, "127.0.0.1", 2998);
+        server.listen();
     }
 }
 
