a3d5e6a1a6ba60028d12aa05ceba03f2a400bf6f
Julian Bokelmann <J.B.Bokelmann@gmail.com>
2022-04-22 13:31:42 +0200

    :sparkles: add reset method for pool workers

--- src/main/java/utils/WorkerPool.java
+++ src/main/java/utils/WorkerPool.java
@@ -9,10 +9,12 @@ import java.util.ArrayList;
 
 public class WorkerPool {
     final ArrayList<Worker> pool;
+    final WorkerParameters workerParameters;
     int nextWorkerId = 0;
 
     public WorkerPool(final int count, final WorkerParameters workerParameters, final Context context) throws ControlException {
         this.pool = new ArrayList<>();
+        this.workerParameters = workerParameters;
         for (int i = 0; i < count; i++) {
             this.pool.add(context.createWorker(workerParameters));
         }
@@ -25,5 +27,15 @@ public class WorkerPool {
 
     private void incrementID() {
         this.nextWorkerId = (this.nextWorkerId + 1) % this.pool.size();
+    }
+
+    public void resetWorker(final Worker currentWorker, final Context context) {
+        final int index = pool.indexOf(currentWorker);
+        pool.get(index).close();
+        try {
+            pool.set(index, context.createWorker(workerParameters));
+        } catch (final ControlException e) {
+            pool.remove(index);
+        }
     }
 }
