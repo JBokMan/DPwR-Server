5b14b0cb8aa64f490961f8f3afd3dc817be57cf1
Julian B <inujal3@gmail.com>
2022-07-24 22:26:10 +0200

    ♻️✅ update test parameters

--- src/test/java/server/DPwRServerTest.java
+++ src/test/java/server/DPwRServerTest.java
@@ -10,10 +10,9 @@ class DPwRServerTest {
     void listen() {
         final InetSocketAddress listenAddress = new InetSocketAddress("127.0.0.1", 2998);
         final int plasmaStoreSize = 1000;
-        final int plasmaTimeout = 500;
         final int clientTimeout = 500;
         final int workerCount = 8;
-        final boolean verbose = false;
+        final boolean verbose = true;
         final DPwRServer server = new DPwRServer(listenAddress, plasmaStoreSize, clientTimeout, workerCount, verbose);
         server.listen();
     }
