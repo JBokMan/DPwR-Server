5a20aa2dd6728b8ff415cc630ea2a864340d2bdc
Julian B <inujal3@gmail.com>
2022-07-30 09:13:46 +0200

    🧪 add test for secondary server

--- src/test/java/server/DPwRServerTest.java
+++ src/test/java/server/DPwRServerTest.java
@@ -1,19 +1,34 @@
 package server;
 
+import de.hhu.bsinfo.infinileap.binding.ControlException;
 import org.junit.jupiter.api.Test;
 
+import java.net.ConnectException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.TimeoutException;
 
 class DPwRServerTest {
 
     @Test
-    void listen() {
+    void listenAsMain() {
         final InetSocketAddress listenAddress = new InetSocketAddress("127.0.0.1", 2998);
         final int plasmaStoreSize = 1000;
         final int clientTimeout = 500;
         final int workerCount = 8;
         final boolean verbose = true;
         final DPwRServer server = new DPwRServer(listenAddress, plasmaStoreSize, clientTimeout, workerCount, verbose);
+        server.listen();
+    }
+
+    @Test
+    void listenAsSecondary() throws ControlException, TimeoutException, ConnectException {
+        final InetSocketAddress listenAddress = new InetSocketAddress("127.0.0.1", 2997);
+        final int plasmaStoreSize = 1000;
+        final int clientTimeout = 500;
+        final int workerCount = 8;
+        final boolean verbose = true;
+        final InetSocketAddress mainServerAddress = new InetSocketAddress("127.0.0.1", 2998);
+        final DPwRServer server = new DPwRServer(listenAddress, mainServerAddress, plasmaStoreSize, clientTimeout, workerCount, verbose);
         server.listen();
     }
 }
